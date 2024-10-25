5b9bc73e5dcbbde69fe510000ea453719cc195f7
Alessandro Dias <acdias83@gmail.com>
2024-10-24 21:36:38 -0300

    inicio js

---
+++ chatUol/javascript/script.js
@@ -0,0 +1,7 @@
+const UUID = "3e3ba45d-0f81-41fa-88e5-5544e02fe5b3";
+let username = prompt("Enter your name:");
+let recipient = 'Todos'; 
+let messageType = 'public'; 
+let currentMessageType = 'public';
+let currentRecipient = 'Todos';
+
